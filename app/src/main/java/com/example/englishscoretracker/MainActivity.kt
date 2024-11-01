fc7b33f6793b88e8737b30b29748a17ea225acf5
Yuki Naka <yukinaka2168@au.com>
2024-11-01 09:50:57 +0900

    made develop branch.

--- app/src/main/java/com/example/englishscoretracker/MainActivity.kt
+++ app/src/main/java/com/example/englishscoretracker/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("develop branch")
                 }
             }
         }
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "made $name",
         modifier = modifier
     )
 }
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     EnglishScoreTrackerTheme {
-        Greeting("Android")
+        Greeting("develop branch")
     }
 }
