bcc9efb942847f29f6df508c23dda986143b6f54
Yuki Naka <yukinaka2168@au.com>
2024-11-01 13:40:24 +0900

    made data class whose name is TypeOfTest.

---
+++ app/src/main/java/com/example/englishscoretracker/TypeOfTest.kt
@@ -0,0 +1,15 @@
+package com.example.englishscoretracker
+
+data class TypeOfTest(
+    val color: Int,
+    val name: String
+)
+
+val typeOfTest = listOf(
+    TypeOfTest(0xFFD32F2F.toInt(), "英検一次"),
+    TypeOfTest(0xFF7B1FA2.toInt(), "英検二次"),
+    TypeOfTest(0xFF1976D2.toInt(), "TOEIC"),
+    TypeOfTest(0xFF388E3C.toInt(), "TOEIC SW"),
+    TypeOfTest(0xFFFBC02D.toInt(), "TOEFL iBT"),
+    TypeOfTest(0xFFF57C00.toInt(), "IELTS"),
+)
