ed7ea1024be2733210b91091c0ac1b28ff906101
Yuki Naka <yukinaka2168@au.com>
2024-11-01 10:05:53 +0900

    made RecordFragment.

---
+++ app/src/main/java/com/example/englishscoretracker/RecordFragment.kt
@@ -0,0 +1,32 @@
+package com.example.englishscoretracker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class RecordFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val composeView = ComposeView(requireContext()).apply {
+            setContent {
+                RecordScreen()
+            }
+        }
+        return composeView
+    }
+}
+
+@Composable
+fun RecordScreen() {
+    Column {
+        Text(text = "記録画面です")
+    }
+}
