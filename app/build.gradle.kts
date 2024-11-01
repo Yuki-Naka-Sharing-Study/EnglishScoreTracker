03b0e746461a111024032a0f120a63e42515296d
Yuki Naka <yukinaka2168@au.com>
2024-11-01 10:02:15 +0900

    implemented navigation. made BottomNavigationBar and function EnglishScoreTracker in MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,12 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.material)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.material3.android)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.navigation.compose.v)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -58,7 +63,6 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
