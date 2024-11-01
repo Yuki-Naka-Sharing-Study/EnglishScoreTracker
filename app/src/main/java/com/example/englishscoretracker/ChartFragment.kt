79aad7239102e4eaffa378228f2d8a30d348447f
Yuki Naka <yukinaka2168@au.com>
2024-11-01 11:49:00 +0900

    made function NoRecordImageView and NoRecordImageViewPreview.

--- app/src/main/java/com/example/englishscoretracker/ChartFragment.kt
+++ app/src/main/java/com/example/englishscoretracker/ChartFragment.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
 import com.example.englishscoretracker.ui.theme.EnglishScoreTrackerTheme
@@ -38,5 +45,25 @@ fun ChartScreen() {
 private fun ChartScreenPreview() {
     EnglishScoreTrackerTheme {
         ChartScreen()
+    }
+}
+
+// 以下は「記録無し」のコード
+@Composable
+private fun NoRecordImageView(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.no_record),
+        contentDescription = stringResource(id = R.string.description_of_the_image),
+        modifier = modifier
+            .size((dimensionResource(id = R.dimen.no_record_image_view)))
+            .aspectRatio(1f)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun NoRecordImageViewPreview() {
+    EnglishScoreTrackerTheme {
+        NoRecordImageView(modifier = Modifier)
     }
 }
